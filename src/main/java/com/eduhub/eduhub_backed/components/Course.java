15f6366a7147caa17aa3b546699e8a17ebcbe056
jeevasanjaysv <jeevasanjay.s2024ece@sece.ac.in>
2026-05-26 15:53:49 +0530

    course component createdd

--- src/main/java/com/eduhub/eduhub_backed/components/Course.java
+++ src/main/java/com/eduhub/eduhub_backed/components/Course.java
@@ -3,8 +3,42 @@ package com.eduhub.eduhub_backed.components;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CourseService {
-    public String getCourse(){
-        return "java-spring-boo";
+public class Course {
+    private int courseCode;
+    private  String courseName;
+    private  int credits;
+
+    public Course(int courseCode, String courseName, int credits) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.credits = credits;
+    }
+
+    public Course() {
+    }
+
+
+    public int getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(int courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
     }
 }
