213c9a3b39e8fa03bf0b7a48297b22c4751b6330
jeevasanjaysv <jeevasanjay.s2024ece@sece.ac.in>
2026-05-28 19:26:06 +0530

    Exceptions file

--- src/main/java/com/eduhub/eduhub_backed/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/eduhub/eduhub_backed/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.eduhub.eduhub_backed.exceptions;
 
-public class ResourseNotFoundException extends RuntimeException {
-  public ResourseNotFoundException(String message) {
-    super(message);
-  }
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, String field, int error) {
+        super(String.format("%s not  found with %s:%s",resource,field,error));
+    }
 }
